eea2cfbe9d5ca5ec913fb4dd7e392234895653a4
Webdesignwill <will@webdesignwill.co.uk>
2014-11-23 18:01:15 +0100

    TODO with additional methods

--- stores/mongoose_backend.js
+++ stores/mongoose_backend.js
@@ -37,6 +37,7 @@ store.on('revocation', function (cmd, cb) {
 });
 
 store.on('verb-question', function (cmd, cb) {
+    console.log('verb-question');
     RelationModel.findOne({
         context: cmd.ctx.name,
         subject: cmd.subject,
@@ -59,6 +60,7 @@ store.on('verb-question', function (cmd, cb) {
 });
 
 store.on('role-question', function (cmd, cb) {
+    console.log('role-question');
     RelationModel.findOne({
         context: cmd.ctx.name,
         subject: cmd.subject,
@@ -83,6 +85,7 @@ store.on('role-question', function (cmd, cb) {
 });
 
 store.on('verb-request', function (cmd, cb) {
+    console.log('verb-request');
     RelationModel.find({
         context: cmd.ctx.name,
         subject: cmd.subject,
@@ -96,6 +99,7 @@ store.on('verb-request', function (cmd, cb) {
 });
 
 store.on('role-request', function (cmd, cb) {
+    console.log('role-request');
     RelationModel.find({
         context: cmd.ctx.name,
         subject: cmd.subject,
@@ -109,6 +113,7 @@ store.on('role-request', function (cmd, cb) {
 });
 
 store.on('verb-subject-request', function (cmd, cb) {
+    console.log('verb-subject-request');
     RelationModel.find({
         context: cmd.ctx.name,
         object: cmd.object,
@@ -121,6 +126,7 @@ store.on('verb-subject-request', function (cmd, cb) {
 });
 
 store.on('role-subject-request', function (cmd, cb) {
+    console.log('role-subject-request');
     RelationModel.find({
         context: cmd.ctx.name,
         object: cmd.object,
@@ -133,6 +139,7 @@ store.on('role-subject-request', function (cmd, cb) {
 });
 
 store.on('object-verb-request', function (cmd, cb) {
+    console.log('object-verb-request');
     RelationModel.find({
         context: cmd.ctx.name,
         object: cmd.object,
@@ -144,6 +151,45 @@ store.on('object-verb-request', function (cmd, cb) {
             return verbs.concat( cmd.ctx.roles[row.role] || [] );
         }, []));
     });
+});
+
+// TODO
+store.on('object-role-map-request', function (cmd, cb) {
+    console.log('object-role-map-request');
+  // var subject = initSubject(cmd);
+  // var map = {};
+  // map[''] = Object.keys(subject.roles || {}).filter(function (role) {
+  //   return subject.roles[role];
+  // });
+  // cb(null, Object.keys(subject.objects || {}).reduce(function (map, object) {
+  //   var roles = Object.keys(subject.objects[object] || {}).filter(function (role) {
+  //     return subject.objects[object][role];
+  //   });
+  //   if (roles.length) map[object] = roles;
+  //   return map;
+  // }, map));
+});
+
+store.on('subject-role-map-request', function (cmd, cb) {
+    console.log('subject-role-map-request');
+  // var subjects = Object.keys(contexts[cmd.ctx.name] || {});
+  // cb(null, subjects.reduce(function (map, subjectName) {
+  //   var subject = initSubject({ subject: subjectName, ctx: cmd.ctx })
+  //     , roles;
+  //   if (cmd.object) {
+  //     var object = (subject.objects || {})[cmd.object] || {};
+  //     roles = Object.keys(object).filter(function (role) {
+  //       return object[role];
+  //     });
+  //   }
+  //   else {
+  //     roles = Object.keys(subject.roles || {}).filter(function (role) {
+  //       return subject.roles[role];
+  //     });
+  //   }
+  //   if (roles.length) map[subjectName] = roles;
+  //   return map;
+  // }, {}));
 });
 
 store.on('reset', function (cb) {
